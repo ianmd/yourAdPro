97ce3598cf08f7d5554d00d6bfad10a6c1fd3a47
Mohammad Alam <najmi.alam@yahoo.com>
2019-11-17 18:00:19 -0800

    all js restored

--- js/main.js
+++ js/main.js
@@ -11,9 +11,46 @@ $(document).ready(function(){
 	});
 
 
+
+
+	$("#clients-logo").owlCarousel({
+		autoPlay: 3000,
+		items : 5,
+		itemsDesktop : [1199,5],
+		itemsDesktopSmall : [979,5],
+	});
+
+	$("#works-logo").owlCarousel({
+		autoPlay: 3000,
+		items : 5,
+		itemsDesktop : [1199,5],
+		itemsDesktopSmall : [979,5],
+	});
+
+
+	// google map
+		var map;
+		function initMap() {
+		  map = new google.maps.Map(document.getElementById('map'), {
+		    center: {lat: -34.397, lng: 150.644},
+		    zoom: 8
+		  });
+		}
+
+
+	// Counter
+
+	$('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+
+
+
 $(function(){
         $(".element").typed({
-            strings: ["Content Creation", "Link Building", "Content Strategy"],
+            strings: ["Content Creation", "Reputation Management", "Content Strategy"],
             typeSpeed: 0
         });
     });
