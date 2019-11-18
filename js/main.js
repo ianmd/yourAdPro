f4fa7348e8806cb0350f7320d6928dd1d04d760a
Mohammad Alam <najmi.alam@yahoo.com>
2019-11-17 17:40:40 -0800

    Unused Js removed and document ready changed to window load

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(window).load(function(){
 
 
 	$("#portfolio-contant-active").mixItUp();
@@ -9,43 +9,6 @@ $(document).ready(function(){
 	    singleItem:true,
 	    autoPlay: 3000,
 	});
-
-
-
-
-	$("#clients-logo").owlCarousel({
-		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
-	});
-
-	$("#works-logo").owlCarousel({
-		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
-	});
-
-
-	// google map
-		var map;
-		function initMap() {
-		  map = new google.maps.Map(document.getElementById('map'), {
-		    center: {lat: -34.397, lng: 150.644},
-		    zoom: 8
-		  });
-		}
-
-
-	// Counter
-
-	$('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
-
-
 
 
 $(function(){
